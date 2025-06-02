551fff5178a1ab908f6ff1a6cb0f7b861881e79c
waleedAbdullah_personalAccount <waleedjadoon840@gmail.com>
2025-06-02 11:25:40 +0500

    squash the message 1 to 3

--- src/components/NewFeature.jsx
+++ src/components/NewFeature.jsx
@@ -2,3 +2,8 @@ import React from "react";
 
 console.log('commit-2')
 console.log('commit-3')
+console.log('commit-4')
+
+console.log('message-01')
+console.log('message-02')
+console.log('message-03')
